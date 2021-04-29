b0329d58ff1171f7b3d191336f3763ca3e9365fd
Casey Nord <nordcw@gmail.com>
2021-04-29 16:48:02 -0700

    unit tests for difference method

--- calculator_unit_tests.py
+++ calculator_unit_tests.py
@@ -16,10 +16,17 @@ class CalculatorUnitTests(unittest.TestCase):
         self.assertEqual(calc.sum(5, 6), 11)
         self.assertEqual(calc.sum(5, -6), -1)
     
+    def test_correct_values_sum(self):
+        calc = calculator.Calculator()
+        self.assertEqual(calc.difference(1, 1), 0)
+        self.assertEqual(calc.difference(5, 6), -1)
+        self.assertEqual(calc.difference(5, -6), 11)
+    
     def test_type(self):
         calc = calculator.Calculator()
         with self.assertRaises(TypeError):
             calc.sum("4", 2)
+            calc.difference("4", 2)
 
 
 if __name__ == '__main__':
