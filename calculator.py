7c618a8fb3d623cc4f1698eb3cf4b34a8f283055
Casey Nord <nordcw@gmail.com>
2021-04-29 16:44:40 -0700

    simple calculator class

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# cs362 - in class activity 03
+# casey nord
+# spring 2021
+
+
+class Calculator:
+
+    def sum(self, a, b):
+        return a + b
+
+    def difference(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        return a / b
