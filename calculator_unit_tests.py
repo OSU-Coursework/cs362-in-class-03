99b0291d59aa67eb04e2c7e7840e9aeedad3dd2a
Casey Nord <nordcw@gmail.com>
2021-04-29 16:50:38 -0700

    fixing test names

--- calculator_unit_tests.py
+++ calculator_unit_tests.py
@@ -9,20 +9,19 @@ import unittest
 
 class CalculatorUnitTests(unittest.TestCase):
 
-
     def test_correct_values_sum(self):
         calc = calculator.Calculator()
         self.assertEqual(calc.sum(1, 1), 2)
         self.assertEqual(calc.sum(5, 6), 11)
         self.assertEqual(calc.sum(5, -6), -1)
     
-    def test_correct_values_sum(self):
+    def test_correct_values_difference(self):
         calc = calculator.Calculator()
         self.assertEqual(calc.difference(1, 1), 0)
         self.assertEqual(calc.difference(5, 6), -1)
         self.assertEqual(calc.difference(5, -6), 11)
     
-    def test_correct_values_sum(self):
+    def test_correct_values_multiply(self):
         calc = calculator.Calculator()
         self.assertEqual(calc.multiply(1, 1), 1)
         self.assertEqual(calc.multiply(5, 6), 30)
